2a98117ff497d247d02788e700c59b13adc9995e
anurag41682 <anuragyadav41682@gmail.com>
2024-01-10 15:16:56 +0530

    Changed Port to AWS ec2

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // const URL = "http://localhost:3001/";
 // const URL = "https://to-do-backend-4qx7.onrender.com/";
-const URL = "https://anur4g.azurewebsites.net/";
+// const URL = "https://anur4g.azurewebsites.net/";
+const URL = "http://3.110.118.231:8080/";
 const API = axios.create({ baseURL: URL });
 
 export const addTask = (data) => {
