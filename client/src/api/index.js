c1f1d80eff0ab8a3a75ad5a7fb911fa2a13e0b8b
anurag41682 <anuragyadav41682@gmail.com>
2024-01-25 19:24:24 +0530

    Updated Backend URL and ec2 URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const URL = "http://localhost:3001/";
 // const URL = "https://to-do-backend-4qx7.onrender.com/";
 // const URL = "https://anur4g.azurewebsites.net/";
-const URL = "http://3.110.118.231:8080/";
+const URL = "https://todo.anur4g.me/";
 const API = axios.create({ baseURL: URL });
 
 export const addTask = (data) => {
